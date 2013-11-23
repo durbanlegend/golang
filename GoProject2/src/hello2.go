d5350f8c712b246e9246de1d3aeb821f4334db2a
durbanlegend <dforbes@iafrica.com>
2013-11-23 22:13:17 +0200

    Go project 2

---
+++ GoProject2/src/hello2.go
@@ -0,0 +1,16 @@
+package main 
+
+import (
+    "flag"
+    "fmt"
+)
+
+const APP_VERSION = "0.1"
+
+// The flag package provides a default help printer via -h switch
+var versionFlag *bool = flag.Bool("v", false, "Print the version number.")
+
+func main() {
+    fmt.Println("Version:", APP_VERSION)
+}
+
